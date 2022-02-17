b0dd9b0505997d884beefeecb0c19980a39f3e04
Floriane Hibelot <floriane@wemaintain.com>
2022-02-17 17:03:52 +0100

    movies to watch

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'bookmark/new'
+  get 'bookmark/destroy'
+  # get 'lists/new'
+  # get 'lists/index'
+  # get 'lists/show'
+  # get 'lists/create'
+  resources :lists, only: [:new, :index, :show, :create] do 
+    resources :bookmarks, only: [ :new, :create ]
+  end
+  resources :bookmarks, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "json"
+require "open-uri"
+
+puts "creating movies ..."
+url = "http://tmdb.lewagon.com/movie/top_rated"
+movies = JSON.parse(URI.open(url).read)['results']
+movies.first(20).each do |movie|
+  url_poster = "https://image.tmdb.org/t/p/w500"
+  Movie.create(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "#{url_poster}#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+    )
+end
+
+puts "movies created! ..."
+
+
